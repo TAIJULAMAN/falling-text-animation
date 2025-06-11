153dde00fdf148b6a452f698b193d91ec5db2d54
Md Shah Aman Patwary <taijulaman8@gmail.com>
2025-06-11 09:52:29 +0600

    readme file added

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,41 +2,41 @@ import React from 'react';
 import { FallingText } from './falling-interactive-text'
 
 function OnHover() {
-  return (
-    <div className="">
-      <div className="w-full h-[calc(100dvh-4rem)]">
-        <FallingText
-          text={`a animated and interactive React components designed by Aman to streamline UI development and simplify your workflow.`}
-          highlightWords={['animated', 'Aman']}
-          trigger="hover"
-          backgroundColor="transparent"
-          wireframes={false}
-          gravity={0.56}
-          fontSize="2rem"
-          mouseConstraintStiffness={0.9}
-        />
-      </div>
-    </div>
-  );
+          return (
+                    <div className="">
+                              <div className="w-full h-[calc(100dvh-4rem)]">
+                                        <FallingText
+                                                  text={`an animated and interactive React components designed by Aman to streamline UI development and simplify your workflow.`}
+                                                  highlightWords={['animated', 'Aman']}
+                                                  trigger="hover"
+                                                  backgroundColor="transparent"
+                                                  wireframes={false}
+                                                  gravity={0.56}
+                                                  fontSize="2rem"
+                                                  mouseConstraintStiffness={0.9}
+                                        />
+                              </div>
+                    </div>
+          );
 }
 
 function OnClick() {
-  return (
-    <div className="">
-      <div className="w-full h-[calc(100dvh-4rem)]">
-        <FallingText
-          text={`React Bits is a library of animated and interactive React components designed to streamline UI development and simplify your workflow.`}
-          highlightWords={['React', 'Bits', 'animated', 'components', 'simplify']}
-          trigger="click"
-          backgroundColor="transparent"
-          wireframes={false}
-          gravity={0.56}
-          fontSize="2rem"
-          mouseConstraintStiffness={0.9}
-        />
-      </div>
-    </div>
-  );
+          return (
+                    <div className="">
+                              <div className="w-full h-[calc(100dvh-4rem)]">
+                                        <FallingText
+                                                  text={`an animated and interactive React components designed by Aman to streamline UI development and simplify your workflow.`}
+                                                  highlightWords={['animated', 'Aman']}
+                                                  trigger="click"
+                                                  backgroundColor="transparent"
+                                                  wireframes={false}
+                                                  gravity={0.56}
+                                                  fontSize="2rem"
+                                                  mouseConstraintStiffness={0.9}
+                                        />
+                              </div>
+                    </div>
+          );
 }
 
 export { OnClick, OnHover }
